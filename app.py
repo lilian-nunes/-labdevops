cc268262768e3a70a0c309666f2bc1800282c89c
lilian-nunes <80015926+lilian-nunes@users.noreply.github.com>
2022-02-23 22:22:07 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-import os
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Lilian"
+    return "Hello World - Lilian - Protegido de CSRF"
 
 if __name__ == '__main__':
-    port = os.getenv('PORT')
-    app.run('0.0.0.0', port=port)
+    app.run()
+    
